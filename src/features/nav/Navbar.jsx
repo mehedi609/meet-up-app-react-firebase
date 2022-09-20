2038f81cb231fa7516335ed7174e987e9f5802fd
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2022-09-20 11:11:31 +0600

    Implemented fake authentication to show different menus

--- src/features/nav/Navbar.jsx
+++ src/features/nav/Navbar.jsx
@@ -1,27 +1,34 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import SignedOutMenu from './SignedOutMenu';
+import { useState } from 'react';
+import SignedInMenu from './SignedInMenu';
 
 export default function NavBar({ setFormOpen }) {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push('/');
+  }
+
   return (
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item as={NavLink} exact to="/" header>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
-          Re-vents
+          Meet-UP
         </Menu.Item>
         <Menu.Item as={NavLink} exact to="/events" name="Events" />
         <Menu.Item as={NavLink} exact to="/createEvent">
           <Button positive inverted content="Create Event" />
         </Menu.Item>
-        <Menu.Item position="right">
-          <Button basic inverted content="Login" />
-          <Button
-            basic
-            inverted
-            content="Register"
-            style={{ marginLeft: '0.5em' }}
-          />
-        </Menu.Item>
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
       </Container>
     </Menu>
   );
